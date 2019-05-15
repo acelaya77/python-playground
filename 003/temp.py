8a146198b14d15261eaab7d8e22f83c13f3125f9
Anthony J. Celaya <anthony.celaya@gmail.com>
2019-05-14 20:52:51 -0700

    fix

--- 003/temp.py
+++ 003/temp.py
@@ -4,9 +4,8 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+from bs4 import BeautifulSoup as bs
 import requests
-import from bs4 beautifulsoup as bs
 
 
 url = "https://www.yachtworld.com/core/listing/cache/searchResults.jsp?cit=true&slim=quick&ybw=&sm=3&searchtype=advancedsearch&Ntk=boatsEN&Ntt=&is=&man=&hmid=0&ftid=0&enid=0&type=%28Sail%29&fromLength=22&toLength=48&fromYear=&toYear=&fromPrice=0&toPrice=300000&luom=126&currencyid=100&city=&pbsint=&boatsAddedSelected=-1&fracts=1"
